d7bd138b0faceb71bc3b7b29e49589f4f3ae1fd0
jtreagor <jtreagor@gmail.com>
2022-07-19 09:02:08 -0400

    creating and migrating the User model

--- rdproj/rdapp/models.py
+++ rdproj/rdapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.forms import CharField
+import string
+import random
 
 # Create your models here.
+
+
+def generate_unique_id():
+    length = 5
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if User.objects.filter(code=code).count() == 0:
+            break
+    return code
+
+
+class User(models.Model):
+    # id is already a part of the Model class!
+    name = models.CharField(max_length=30, null=False)
+    username = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=30, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    adminQ = models.BooleanField(null=False, default=False)
